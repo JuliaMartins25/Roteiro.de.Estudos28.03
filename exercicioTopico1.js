a06035bf8e25f3dbab11a45a6ddddb62bf01a968
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-03-28 09:56:13 -0300

    exercio tópico 1

---
+++ exercicioTopico1.js
@@ -0,0 +1,19 @@
+//Declaro váriavel para idades
+let idadeA = 19;
+let idadeB = 10; 
+
+//Verificar se a idade A é ideal para dirigir
+if (idadeA >= 18){
+    console.log('Idade A é adequada para dirigir');
+}else{
+    console.log('É idade não adequada para dirigir');
+}
+
+//Verificar se a idade B é ideal para dirigir
+if (idadeB >= 18){
+    console.log('Idade B é adequada para dirigir');
+}else{
+    console.log('Idade B não é adequada para dirigir');
+}
+
+
