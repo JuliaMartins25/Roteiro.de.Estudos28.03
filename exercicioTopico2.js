9ca31f91de33fa4efbf61ed773710400277418bf
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-03-28 10:20:44 -0300

    exercicio tópico 2

---
+++ exercicioTopico2.js
@@ -0,0 +1,20 @@
+//estruturas de repetição
+
+//Declaro variável A para exibir sequência de números
+console.log('sequencia utilizando while:')
+let a = 1;
+
+//estrutura de repetição WHILE para executar a sequência
+while ( a <= 10){
+    console.log('sequencia completa:' + a);
+    a++
+}
+
+//Declaro outra variavel para exibir sequência de números
+console.log('sequencia utilizando FOR:')
+let b = 1;
+
+//estrutura de repetição FOR para executar a sequência
+for (let i = b; i <= 10; i++){
+    console.log('sequencia completa:' + i);
+}
