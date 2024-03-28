79bf8725938a0e41c1393ac0cc61302d35c2aa39
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-03-28 10:22:10 -0300

    exercicio tópico 3 quetão 1

---
+++ exercicioTopico3p1.js
@@ -0,0 +1,9 @@
+//declaro variavel para Array
+let array = [2, 5, 9, 3, 10];
+
+//Exibir todos os elementos do array no console
+console.log(array);
+
+//Exibir quantos elementos tem no array
+array.length
+console.log('o array tem' +  array.length  + 'elementos');
